c93368ec776eb5fe5bc63d30bc6ce561965cf3be
jonathans <jonathan.silkewall@gmail.com>
2020-09-13 20:56:08 +0200

    init

---
+++ todo_service/app/model/todo_model.py
@@ -0,0 +1,31 @@
+from app import db
+from marshmallow import fields, Schema
+
+class Todo (db.Model):
+    id = db.Column(db.Integer, primary_key=true)
+    task = db.Column(db.Text())
+    owner = db.Column(db.String(255))
+
+    
+    def __init__(self, data):
+        self.id = data.get('id')
+        self.task = data.get('task')
+        self.owner = data.get('owner')
+
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+        return self.id
+
+    @classmethod
+    def getTask(cls, id)
+
+        return cls.query.get(id)
+
+
+class TodoSchema (Schema):
+    id = fields.Int(dump_only=True)
+    task = fields.String(dump_only=True)
+    owner = fields.String(dump_only=True)
