a89f3b707ee306aabaefa2671cb6598e313241c1
hrvCode <jonathan.silkewall@gmal.com>
2020-09-16 23:35:47 +0200

    add: user model

--- todo_service/todo/model/todo.py
+++ todo_service/todo/model/todo.py
@@ -1,16 +1,42 @@
 from marshmallow import fields, Schema
 from todo import db
 
-class Todo (db.Model):
-    __tablename__  = "task"
+class User(db.Model):
+    #__tableName__: "User"
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.Text())
-    owner = db.Column(db.String(255))
+    name = db.Column(db.String(100), nullable=False)
+    todo = db.relationship('Todo', backref='creator', lazy=True)
+
+    def __init__(self, data):
+        self.id = data.get('id')
+        self.name = data.get('name')
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+        return self.id
+
+
+    @classmethod
+    def getUser(cls):
+        return cls.query.get(id)
+
+
+
+class Todo (db.Model):
+    # __tablename__  = "Todo"
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.Text(), nullable=False)
+    owner = db.Column(db.String(225))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __init__(self, data):
         self.id = data.get('id')
         self.task = data.get('task')
         self.owner = data.get('owner')
+        self.user_id = data.get('user_id')
 
     def save(self):
         db.session.add(self)
@@ -22,14 +48,25 @@ class Todo (db.Model):
     def getTask(cls, id):
         return cls.query.get(id)
 
-    @classmethod
-    def getAllTasks(cls):
-        k = db.session.query(cls).all()
-        print(k)
-        return k
-
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "tsk": self.task,
+            "owner": self.owner,
+            "creator": self.creator
+        }
 
 class TodoSchema (Schema):
     id = fields.Int(dump_only=True)
     task = fields.String(dump_only=True)
-    owner = fields.String(dump_only=True)
+
+class UserSchema (Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.String(dump_only=True)
+    todos = fields.String(dump_only=True)
+    task = fields.Nested(TodoSchema, only=["task"])
+
+
+
+
--- todo_service/todo/routes.py
+++ todo_service/todo/routes.py
@@ -1,7 +1,7 @@
 from todo import app
-from flask import make_response
-from flask import request
-from todo.model.todo import Todo
+from flask import make_response, request, jsonify
+from todo.model.todo import Todo, User
+import json
 
 @app.route('/', methods=['get'])
 def index():
@@ -11,36 +11,56 @@ def index():
 
 @app.route('/api/todo/', methods=['get'])
 def getAll():
-    fromDb = Todo.getAllTasks()
-    for todo in fromDb:
-        print(todo)
-   # return make_response(todoData)
+    return jsonify([i.serialize for i in Todo.query.all()])
+
+@app.route('/api/user/todo/<int:user_id>', methods=['get'])
+def getAllTodosByUserId():
+    return jsonify([i.serialize for i in Todo.query.all()])
 
 
-@app.route('/api/todo/<int:post_id>', methods=['get'])
-def getTodo(post_id):
+@app.route('/api/todo/<int:todo_id>', methods=['get'])
+def getTodo(todo_id):
     # getTodo(post_id) // hämtar från dbn
-    return f'todo id {post_id}'
+    return f'todo id {todo_id}'
 
 
 @app.route('/api/todo/', methods=['post'])
 def createTodo():
     if request.method == 'POST':
+        #move to todoservice
         content = request.get_json()
-        em = Todo(content)
-        id = em.save()
-        print(content)
-        return make_response("Id of todo", id)
+        todo = Todo(content)
+        user = User.query.get(1)
+        user.todo.append(todo)
+        user.save()
+        return make_response(f"{user.name} added to do Task: '{todo.task}' shall be executed by: {todo.owner}")
 
 
 
-@app.route('/api/todo/<int:post_id>',  methods=['put'])
-def updateTodo(post_id):
+@app.route('/api/todo/<int:todo_id>',  methods=['put'])
+def updateTodo(todo_id):
     # getTodo(post_id) // hämtar från dbn
-    return f'todo id {post_id}'
+    return f'todo id {todo_id}'
 
 
 @app.route('/api/todo/<int:post_id>', methods=['delete'])
-def deleteTodo(post_id):
+def deleteTodo(todo_id):
     # getTodo(post_id) // hämtar från dbn
-    return f'todo id {post_id}'
+    return f'todo id {todo_id}'
+
+
+
+
+
+## API USERS
+
+@app.route('/api/user', methods=['post'])
+def createUser():
+
+    if request.method == 'POST':
+        #move to User service
+        userData = request.get_json()
+        user = User(userData)
+        user_id = user.save()
+        print(user_id)
+        return make_response(f"you just made a user with id: {user_id}") 
