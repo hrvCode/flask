fdb2ae5a98d6539062ba4ee5492c8efd5657af85
jonathans <jonathan.silkewall@gmail.com>
2020-09-26 21:26:20 +0200

    new db

---
+++ todo_service/migrations/versions/94c3d9f115a9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 94c3d9f115a9
+Revises: 
+Create Date: 2020-09-26 21:20:47.938078
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '94c3d9f115a9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
